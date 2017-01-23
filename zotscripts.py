ee4d7197e9d7bca817f68819fa81fe3d3d178036
akokai <ak@kaios.net>
2017-01-23 01:38:29 -0800

    Add code for tagging items in collections

--- zotscripts.py
+++ zotscripts.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+
+"""Scripts & operations for maintaining my Zotero library using Pyzotero."""
 
 from __future__ import unicode_literals, print_function
 
@@ -11,6 +13,8 @@ from boltons.iterutils import remap
 
 import settings
 
+# Regex patterns for words in a title that shouldn't be lowercased
+# e.g. all-caps, CamelCase, etc.
 CONSERVE_RX = [
     r'(?P<head>\w+)(?(head)(?P<hump>[A-Z0-9][a-z]*)|(?P=hump){2,})',
     r'[A-Z\.]{2,}',  # 'E.U.'
@@ -26,19 +30,70 @@ SEPARATORS = re.compile(r'([\:\?\!\.]\s|\s+\|\s|\s+[-–—]\s|—)')
 
 
 def all_coll_items(zot, *args, **kwargs):
-    '''
+    """
     Lazy generator for all items in a collection.
 
     Modified from: https://gist.github.com/rdhyee/404573708a805861edb5
-    '''
+    """
     for page in chain([zot.collection_items(*args, **kwargs)],
                       zot.iterfollow()):
         for item in page:
             yield item
 
 
+def coll_names_keys(colls_list, *args, **kwargs):
+    """
+    Return dict of names: keys for all collections in the given list.
+
+    Warning: if multiple collections in the list share the same name,
+    only one will be returned.
+    """
+    return {i['data']['name']: i['data']['key'] for i in colls_list}
+
+
+def coll_key_from_name(name, colls_list):
+    """
+    Find the key of a collection by name, from a list of collections.
+
+    Return a list if more than one found, None if none found.
+    """
+    keys = []
+
+    for coll in colls_list:
+        if coll['data']['name'] == name:
+            keys.append(coll['data']['key'])
+
+    if len(keys) == 1:
+        return keys[0]
+    elif len(keys) > 1:
+        return keys
+    else:
+        return None
+
+
+def autotag_subcoll_items(zot, key):
+    subcolls = zot.collections_sub(key)
+    subcoll_names_keys = coll_names_keys(subcolls)
+    for (name, key) in subcoll_names_keys.items():
+        items = all_coll_items(zot, key)
+        for item in items:
+            zot.add_tags(item, name)
+            try:
+                print('Adding tag', name, 'to', item['data']['title'])
+            except KeyError:
+                pass
+
+
+# def add_members_to_coll(zot, from_coll, to_coll, *args, **kwargs):
+#     # Doesn't work because Zotero.collection_items only returns
+#     # top-level items (contrary to docs).
+#     items = all_coll_items(from_coll, *args, **kwargs)
+#     for item in items:
+#         zot.addto_collection(item, to_coll)
+
+
 def get_all_titles(items):
-    '''Get all main titles from a list of Zotero items.'''
+    """Get all main titles from a list of Zotero items."""
     titles = []
 
     def visit(path, key, value):
@@ -51,7 +106,7 @@ def get_all_titles(items):
 
 
 def sentence_case(text):
-    '''Convert a fragmentary phrase to sentence case, somewhat sensibly.'''
+    """Convert a fragmentary phrase to sentence case, somewhat sensibly."""
     if SEPARATORS.match(text):
         return text
 
@@ -76,7 +131,7 @@ def sentence_case(text):
 
 
 def to_sentence_case(title):
-    '''Attempt to sensibly convert a title to sentence case.'''
+    """Attempt to sensibly convert a title to sentence case."""
     if title == '':
         return title
 
@@ -86,12 +141,12 @@ def to_sentence_case(title):
 
 
 def item_titles_to_sentence(item, keys=['title', 'shortTitle', 'bookTitle']):
-    '''
+    """
     Change an item's various titles to sentence case.
 
     Only return the item if it has been changed, otherwise return None.
     Ignore notes and attachments.
-    '''
+    """
     if item['data']['itemType'] in ['attachment', 'note']:
         return None
 
@@ -111,11 +166,11 @@ def item_titles_to_sentence(item, keys=['title', 'shortTitle', 'bookTitle']):
 
 
 def zot_titles_to_sentence(zot, coll_id=settings.TEST_COLL_ID, dry_run=False):
-    '''
+    """
     Get items from a Zotero collection & change their titles to sentence case.
 
     Changes items on the server (or not at all) and does not return anything.
-    '''
+    """
     print(zot.num_collectionitems(coll_id), 'items in collection.')
     items = all_coll_items(zot, coll_id)
 
@@ -126,11 +181,11 @@ def zot_titles_to_sentence(zot, coll_id=settings.TEST_COLL_ID, dry_run=False):
 
 
 def journal_cleanup(item):
-    '''
+    """
     If item is journal article, delete the URL if there's a DOI.
 
     Only return the item if it has been changed, otherwise return None.
-    '''
+    """
     if item['data']['itemType'] != 'journalArticle':
         return None
 
@@ -153,11 +208,11 @@ def journal_cleanup(item):
 
 
 def zot_journal_cleanup(zot, coll_id=settings.TEST_COLL_ID, dry_run=False):
-    '''
+    """
     Get items from a collection & clean up some fields for journal articles.
 
     Changes items on the server (or not at all) and does not return anything.
-    '''
+    """
     print(zot.num_collectionitems(coll_id), 'items in collection.')
     items = all_coll_items(zot, coll_id)
 
@@ -168,9 +223,9 @@ def zot_journal_cleanup(zot, coll_id=settings.TEST_COLL_ID, dry_run=False):
 
 
 def main():
-    '''
+    """
     Perform batch operations on a Zotero collection.
-    '''
+    """
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('-c', '--collection', action='store', type=str,
                         help='collection ID', default=settings.TEST_COLL_ID)
